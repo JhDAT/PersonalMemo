a781f6faae9a89f8b20b1dd8a99172c7b35cbb89
JANGHUIJO <jhjo.tech@gmail.com>
2018-09-03 20:14:35 +0900

    카메라 접근 권한 유/무에 따라 알람창 구분해서 나오도록 함 유 - 카메라 무 - 셋팅화면으로 이동하는 알람창 다음에 셋팅화면으로 이동 초기 - 카메라가 나오면 사용할건지 물어보도록 함

--- PersonalMemo/PersonalMemo/Source/ViewController/MemoFromVC.swift
+++ PersonalMemo/PersonalMemo/Source/ViewController/MemoFromVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MemoFromVC: UIViewController {
     
@@ -19,9 +20,13 @@ class MemoFromVC: UIViewController {
     //MARK: - viewDidLoad
     @IBAction func save(_ sender: Any) {
         guard self.contents.text?.isEmpty == false else {
-            let alert = UIAlertController(title: nil, message: "내용을 입력해주세요", preferredStyle: .alert)
+            let alert = UIAlertController(title: nil,
+                                          message: "내용을 입력해주세요",
+                                          preferredStyle: .alert)
             
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "OK",
+                                          style: .default,
+                                          handler: nil))
             self.present(alert, animated: true)
             return
         }
@@ -41,43 +46,95 @@ class MemoFromVC: UIViewController {
     
     @IBAction func pick(_ sender: Any) {
         let picker = UIImagePickerController()
-        
         picker.delegate = self
-        // 선택한 사진 및 이미지를 수정
-        picker.allowsEditing = true
+        let alert = UIAlertController( title: nil,
+                                       message: "이미지 가져올 곳을 선택해주세요",
+                                       preferredStyle: .actionSheet)
+        let camera = UIAlertAction(title: "카메라",
+                                   style: .default) { [weak self] (action) in
+                                    guard let strongSelf = self else { return }
+                                    
+                                    let status = AVCaptureDevice.authorizationStatus(for: .video)
+                                    switch status {
+                                    case .denied:   // 셋팅에서 접근 권한을 껐을때
+                                        let settingAlert = UIAlertController(
+                                            title: "카메라에 접근권한이 없습니다",
+                                            message: "설정 > PersonalMemo에서 카메라를 활성화해주세요",
+                                            preferredStyle: .alert)
+                                        let showSetting = UIAlertAction(
+                                            title: "설정",
+                                            style: .default,
+                                            handler: { (action) in
+                                                guard let settingURL = URL(
+                                                    string: UIApplicationOpenSettingsURLString),
+                                                    UIApplication.shared.canOpenURL(settingURL)
+                                                    else { return }
+                                                if #available(iOS 10.0, *) {
+                                                    UIApplication.shared.open(settingURL)
+                                                } else {
+                                                    UIApplication.shared.canOpenURL(settingURL)
+                                                }
+                                        })
+                                        let cancel = UIAlertAction(
+                                            title: "확인",
+                                            style: .cancel,
+                                            handler: nil)
+                                        
+                                        for alert in [showSetting, cancel] {
+                                            settingAlert.addAction(alert)
+                                        }
+                                        
+                                        strongSelf.present(
+                                            settingAlert,
+                                            animated: true,
+                                            completion: nil)
+                                    default:
+                                        guard UIImagePickerController.isSourceTypeAvailable(.camera)
+                                            else { return }
+                                        picker.sourceType = .camera
+                                        picker.allowsEditing = true
+                                        strongSelf.present(picker, animated: true)
+                                    }
+        }
         
-        // 이미지 피커 화면 표시
-        self.present(picker, animated:  false)
+        let saveAlbum = UIAlertAction(title: "저장앨범",
+                                      style: .default) { (action) in
+            guard UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum)
+                else { return }
+            // 선택한 사진 및 이미지를 수정
+            picker.sourceType = .savedPhotosAlbum
+            picker.allowsEditing = true
+            // 이미지 피커 화면 표시
+            self.present(picker, animated:  false)
+        }
+        
+        let photoLibrary = UIAlertAction(title: "사진 라이브러리",
+                                         style: .default) { (action) in
+            guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+                else { return }
+            picker.sourceType = .photoLibrary
+            picker.allowsEditing = true
+            self.present(picker, animated: true)
+        }
+        
+        for action in [camera, saveAlbum, photoLibrary] {
+            alert.addAction(action)
+        }
+        present(alert, animated: true)
     }
     
     //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         contents.delegate = self
-        // Do any additional setup after loading the view.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 // MARK: - UIImagePickerControllerDelegate
 extension MemoFromVC : UIImagePickerControllerDelegate {
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
         // 선택된 이미지를 미리보기에 표시
         self.preview.image = info[UIImagePickerControllerEditedImage] as? UIImage
         picker.dismiss(animated: true)
