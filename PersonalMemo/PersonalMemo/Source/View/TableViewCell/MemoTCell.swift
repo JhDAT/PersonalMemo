a5e88bdaa7786aa21e01aede7c3118ecf85ab240
JANGHUIJO <jhjo.tech@gmail.com>
2018-09-03 17:20:17 +0900

    MemoCell에 들어갈 데이터 모델 구현

--- PersonalMemo/PersonalMemo/Source/View/TableViewCell/MemoTCell.swift
+++ PersonalMemo/PersonalMemo/Source/View/TableViewCell/MemoTCell.swift
@@ -10,15 +10,9 @@ import UIKit
 
 class MemoTCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    //MARK: - IBOutlet
+    @IBOutlet weak var subject: UILabel!        // 제목
+    @IBOutlet weak var contents: UILabel!       // 내용
+    @IBOutlet weak var regdate: UILabel!        // 등록일
+    @IBOutlet weak var img: UIImageView!        // 이미지
 }
