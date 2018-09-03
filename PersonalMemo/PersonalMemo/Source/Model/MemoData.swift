d4aa4c2ef23af06d8adc87e0026582934a3deaf1
JANGHUIJO <jhjo.tech@gmail.com>
2018-09-03 15:40:15 +0900

    기능별로 폴더 구분 및 MemoData class 구현, AppDelegate에 memolist 변수 추가

---
+++ PersonalMemo/PersonalMemo/Source/Model/MemoData.swift
@@ -0,0 +1,17 @@
+//
+//  MemoData.swift
+//  PersonalMemo
+//
+//  Created by Jo JANGHUI on 2018. 9. 3..
+//  Copyright © 2018년 JhDAT. All rights reserved.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx : Int?
+    var title : String?
+    var contents : String?
+    var image : UIImage?
+    var regdate : Date?
+}
---
+++ PersonalMemo/PersonalMemo/Source/View/TableViewCell/MemoTCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoTCell.swift
+//  PersonalMemo
+//
+//  Created by Jo JANGHUI on 2018. 9. 3..
+//  Copyright © 2018년 JhDAT. All rights reserved.
+//
+
+import UIKit
+
+class MemoTCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
